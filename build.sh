a340c0d080931e0183e8a0ec5af1db976a72944e
Thomas Showers <showerstm@gmail.com>
2017-11-03 07:05:10 -0300

    Update build.sh
    Include remove-msbuild-nuget in build.

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ DIR=`dirname $0`
 
 # Install msi-file-report
 cp "$DIR/msi-file-report/msi-file-report.ps1" "$DIR/bin/"
+
+# Install remove-msbuild-nuget
+cp "$DIR/remove-msbuild-nuget/remove-msbuild-nuget.sh" "$DIR/bin/"
